f3577a6705eff59e811bf308f5d478199425a9e2
Kaustubhbaviskar2000 <147398863+Kaustubhbaviskar2000@users.noreply.github.com>
2024-01-04 15:06:16 +0530

    Add files via upload

---
+++ HRSolution/HRAPP/Entities/Employee.cs
@@ -0,0 +1,12 @@
+namespace HRAPP.Entities;
+
+[Serializable]
+public class Employee{
+    public int Id{get;set;}
+    public string Name{get;set;}
+
+    public Employee(int id,string name){
+        this.Id=id;
+        this.Name=name;
+    }
+}
---
+++ HRSolution/HRAPP/Program.cs
@@ -0,0 +1,18 @@
+﻿using HRAPP.Entities;
+using HRAPP.Services;
+using HRAPP.Controllers;
+using HRAPP.Views;
+
+IEmployeeService svc=new EmployeeService();
+EmployeesController controller=new EmployeesController(svc);
+List<Employee> model=controller.GetAll();
+ListView view=new ListView(model);
+view.Render();
+Console.WriteLine("Welcome to MVC in C#");
+
+
+Console.WriteLine("enter employees details");
+Employee emp = new Employee(int.Parse(Console.ReadLine()),Console.ReadLine());
+svc.Insert(emp);
+
+view.Render();
